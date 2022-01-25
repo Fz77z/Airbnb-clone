2fbfe878cc1fe9272f0c7227a1d4f58e33b9d7bb
fz77 <kanemcdermott@gmx.com>
2022-01-25 02:43:35 +0000

    Airbnb-Clone

--- components/Banner.js
+++ components/Banner.js
@@ -2,22 +2,34 @@ import Image from 'next/image';
 import React from 'react';
 
 function Banner() {
-  return <div className='relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]'>
+  return <div className='relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[700px] 2xl:h-[1000px]'>
 
-    <Image className="object-[center_bottom] md:object-[center] lg:object-[center_bottom] 2xl:object-[center]"
-          // src="https://i.dawn.com/primary/2020/06/5ef4c35d79b45.jpg"
-            src="https://a0.muscache.com/im/pictures/57b9f708-bb12-498c-bc33-769f8fc43e63.jpg"
+    {/* <Image className="object-[center_bottom] md:object-[center] lg:object-[center_bottom] 2xl:object-[center]"  */}
+
+    <Image className="object-[center_bottom]" 
+            src="https://web.archive.org/web/20210725183154im_/https://a0.muscache.com/im/pictures/e4a2a61c-589f-4e49-b3b8-968a6bc23389.jpg?im_w=2560"
 
         layout='fill'
         objectFit="cover"
         
     />
-    <div className='absolute top-1/2 w-full text-center'>
+
+
+
+
+
+    {/* <div className='absolute top-1/2 w-full text-center'>
       <p className='text-lg :sm:text-xl text-black text-bold drop-shadow-lg lg:text-2xl xl:text-3xl'>
         Not sure where to go? Perfect.
+      </p> */}
+
+
+      <p className='absolute 2xl:top-40 2xl:w-1/2 2xl:left-60 2xl:right-50 2xl:text-8xl xl:top-40 xl:w-1/2 xl:left-36 xl:right-50 xl:text-7xl lg:top-28 lg:w-1/2 lg:left-24 lg:right-50 lg:text-6xl md:top-16 md:w-1/2 md:left-14 md:right-50 md:text-5xl sm:top-16 sm:w-1/2 sm:left-14 sm:right-50 sm:text-4xl top-12 w-1/2 left-12 right-50 text-3xl text-white font-bold shadow-sm	'>
+      Olympian &  <br></br>Paralympian<br></br> Online<br></br> Experiences
       </p>
 
-      <button className='sm:mt-4 text-purple-500 bg-white px-10 py-4 rounded-full shadow-md font-bold my-3 hover:shadow-lg active:scale-90 transistion duration-150'>I'm flexible</button>
+      <div className='absolute 2xl:top-80 2xl:py-60 2xl:left-60 xl:top-52 xl:py-60 xl:left-36 lg:top-28 lg:py-60 lg:left-24 md:top-14 md:py-52 md:left-14 sm:top-20 sm:py-36 sm:left-14 top-12 py-36 left-10 w-full'>
+      <button className='sm:mt-4 text-purple-500 bg-white px-10 py-4 rounded-full shadow-md font-bold my-3 hover:shadow-lg active:scale-90 transistion duration-150 '>Explore Now</button>
       </div>
   </div>;
 }
--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['i.dawn.com','assets.awwwards.com','upload.wikimedia.org','kanemcdermott.com','a0.muscache.com','trendland.com', 'www.selectvacationproperties.com','images.contentstack.io','wallpaperaccess.com','thecamp.co.nz', 'jsonkeeper.com','links.papareact.com'],
+    domains: ['web.archive.org','i.dawn.com','assets.awwwards.com','upload.wikimedia.org','kanemcdermott.com','a0.muscache.com','trendland.com', 'www.selectvacationproperties.com','images.contentstack.io','wallpaperaccess.com','thecamp.co.nz', 'jsonkeeper.com','links.papareact.com'],
   },
 }
